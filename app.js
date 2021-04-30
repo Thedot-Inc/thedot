5047c82a7e77094ccd6b680edcc3987d6d6f80cc
vicky <srivicky2000@gmail.com>
2021-04-30 11:25:36 +0530

    first test

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 const Contact = require('./modules/contact');
 
-mongoose.connect('mongodb://localhost:27017/clients', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Database started");
 })
 
