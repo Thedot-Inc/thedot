cbe8aef886d55ddfa7e755ff9342048fa93854ac
vicky <srivicky2000@gmail.com>
2021-05-03 22:42:09 +0530

    anim

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
+
+// app.get("/all", (req, res) => {
+//     Contact.find({}, (err, done) => {
+//         array a = done;
+
+//     })
+// })
+
 app.get("/contactus", (req, res) => {
     res.render("contact");
 })
